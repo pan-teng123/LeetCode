a1d30ffbafb67353658f47b0383110a48ba6f448
pan-teng123 <3155174850@qq.com>
2024-01-08 17:34:03 +0800

    1.8

--- main.cpp
+++ main.cpp
@@ -1,38 +1,19 @@
 #include <iostream>
 #include <vector>
-#include<string>
+#include <string>
 using namespace std;
-//extern int search(vector<int> &nums, int target);
-//extern int removeElement(vector<int> &nums, int target);
-//extern int sortedSquares(vector<int>& nums);
-//extern int minSubArrayLen(int target, vector<int>& nums);
-//extern vector<vector<int>> generateMatrix(int n);
-//extern bool isAnagram(string s, string t);
-// extern vector<int> twoSum(vector<int>& nums, int target);
-
-// void lc1(){
-//     vector<int>a={2,7,11,15};
-//     int target=9;
-//     vector<int>res= twoSum(a,target);
-//     for (int i = 0; i < res.size(); ++i) {
-//         cout<<res[i]<<endl;
-//     }
-// }
-//extern vector<vector<int>> subsets(vector<int>& nums);
+vector<vector<int>> subsets(vector<int> &nums);
 int main()
 {
-//    vector<int> candidates={0};
-//    vector<vector<int>>res = subsets(candidates);
-//    for (size_t i = 0; i < res.size(); i++)
-//    {
-//        for (int j = 0; j < res[i].size(); ++j) {
-//            cout<<res[i][j];
-//        }
-//        cout<<endl;
-//    }
-    string s="";
-    if(s.empty()){
-        cout<< true;
+    vector<int> candidates = {1, 2, 3};
+    vector<vector<int>> res = subsets(candidates);
+    for (size_t i = 0; i < res.size(); i++)
+    {
+        for (int j = 0; j < res[i].size(); ++j)
+        {
+            cout << res[i][j];
+        }
+        cout << endl;
     }
     return 0;
 }
